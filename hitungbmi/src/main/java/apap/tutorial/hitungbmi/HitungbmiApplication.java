320ec958674c8ce40f71b1cdd100e428c42134cc
Marcel Valdhano <marcelvaldhano@gmail.com>
2020-09-24 17:13:30 +0700

    Selesai

--- hitungbmi/src/main/java/apap/tutorial/hitungbmi/HitungbmiApplication.java
+++ hitungbmi/src/main/java/apap/tutorial/hitungbmi/HitungbmiApplication.java
@@ -1,4 +1,4 @@
-package apaptutorial.hitungbmi;
+package apap.tutorial.hitungbmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hitungbmi/src/test/java/apap/tutorial/hitungbmi/HitungbmiApplicationTests.java
+++ hitungbmi/src/test/java/apap/tutorial/hitungbmi/HitungbmiApplicationTests.java
@@ -1,4 +1,4 @@
-package apaptutorial.hitungbmi;
+package apap.tutorial.hitungbmi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
